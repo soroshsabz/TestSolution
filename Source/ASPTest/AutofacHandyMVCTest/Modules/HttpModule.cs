b63ddbcfc62657e8e615fdd71db1bc321c3f218e
soroshsabz <soorosh_abi@hotmail.com>
2023-11-10 13:12:20 +0330

    Add HttpModule

---
+++ Source/ASPTest/AutofacHandyMVCTest/Modules/HttpModule.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+
+namespace AutofacHandyMVCTest.Modules
+{
+    public class HttpModule : Module
+    {
+        public bool IsEnable { get; set; }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            if (IsEnable)
+            {
+                builder.Register(ctx =>
+                {
+                    var services = new ServiceCollection();
+                    services.AddHttpClient();
+                    var provider = services.BuildServiceProvider();
+                    return provider.GetRequiredService<IHttpClientFactory>()
+                                    .CreateClient("HomeSubscriberServiceHttpClient");
+                }).As<HttpClient>();
+            }
+        }
+    }
+}
